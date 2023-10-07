d6da5b47606a980124b3578ec9c7a0e61a68faef
Dessy1412 <dessyfitriyanidewi14@gmail.com>
2023-10-07 16:49:19 +0700

    Tugas JS 1

---
+++ script.js
@@ -0,0 +1,72 @@
+
+// If, Else, Nested If
+/*let inputBilangan= parseInt(prompt("Masukkan bilangan?"));
+if (inputBilangan >= 10){
+    alert("Angka lebih besar dari 10");
+}else if (inputBilangan <= 10){
+    alert("Angka lebih kecil dari 10");
+}else{
+    alert("Ini bukan angka");
+}*/
+
+
+// Switch Case
+/*let inputHari = prompt("Masukkan kapan anda libur?");
+switch(inputHari){
+    case "senin":
+    case "selasa":
+    case "rabu":
+    case "kamis":
+    case "jumat":
+        alert("Terus Semangat");
+        break;
+    case "sabtu":
+    case "minggu":
+        alert("Happy Weekend");
+        break;
+    default:
+        alert("Anda salah memasukkan hari libur");
+        break;
+}*/
+
+
+// For Statement
+/*let s = "";
+let size = 0;
+
+for(let i = 10; i > size; i--){
+    for(let j = 0; j < i; j++){
+        s += "* ";
+    
+    s += "\n";
+}
+console.log(s);*/
+
+
+// While, do While
+//While
+/*let jmlAngka = 20;
+let angka = 0;
+while (angka <= jmlAngka){
+    console.log("Ini adalah angka " + angka);
+    angka++;
+    }*/
+
+//do while
+/*do{
+    angka += 1;
+    console.log("Ini merupakan angka " + angka);
+}while (angka < 10);*/
+
+// Function Statement
+/*function jmlVolumKubus (a, b){
+    let hasil;
+    let volumA = a * a * a;
+    let volumB = b * b * b;
+
+    hasil = volumA + volumB;
+
+    return hasil;
+}
+console.log(jmlVolumKubus(2, 3));*/
+
